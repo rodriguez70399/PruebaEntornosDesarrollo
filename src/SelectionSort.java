5d61f4fa81d2470718e3b6ac9ad0ee8c4fdf39d9
Jose Manuel <rodriguez70399@gmail.com>
2022-02-03 04:45:33 +0100

    Commit Inicial

---
+++ src/SelectionSort.java
@@ -0,0 +1,41 @@
+public class SelectionSort
+{
+    public void sortArray(int[] array) {
+		for (int i = array.length - 1; i > 0; i--) {
+			int maxValue = 0;
+			for (int j = 0; j < i; j++) {
+				if (array[j + 1] > array[maxValue]) {
+					maxValue = j + 1;
+				}
+			}
+			swap(array, i, maxValue);
+			printArray(array);
+		}
+	}
+	
+	public int[] sortArray2(int[] numbers) {
+		for (int i=0; i < numbers.length - 1;i++) {
+			for (int j=i+1;j<numbers.length;j++) {
+				if (numbers[i]>numbers[j]) {
+					int auxiliar=numbers[i];
+					numbers[i]=numbers[j];
+					numbers[j]=auxiliar;
+				}
+			}
+		}
+		return numbers;
+	}
+
+	public void printArray(int[] array) {
+		for (int i = 0; i < array.length; i++) {
+			System.out.printf("%d \t", array[i]);
+		}
+		System.out.println();
+	}
+
+	public void swap(int[] array, int a, int b) {
+		int value = array[b];
+		array[b] = array[a];
+		array[a] = value;
+	}
+}
